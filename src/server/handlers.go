6b3ce10288c66d01189ef68096d9d2357221204c
Blanka 3719 <mmohammed>
2026-06-09 17:02:18 +0200

    17

--- src/server/handlers.go
+++ src/server/handlers.go
@@ -6,7 +6,27 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello forum"))
+
+	data := struct {
+		Name       string
+		Categories []string
+	}{
+		Name: "Le Dojo",
+		Categories: []string{
+			"Chat général",
+			"MMA",
+			"Boxe Anglaise",
+			"Muay Thai",
+			"Jujitsu Brésilien",
+			"Grappling",
+			"Autres sports de combat",
+		},
+	}
+
+	err := authTemplates.ExecuteTemplate(w, "index.html", data)
+	if err != nil {
+		http.Error(w, "Template error: "+err.Error(), 500)
+	}
 }
 
 func chatHandler(w http.ResponseWriter, r *http.Request) {
