23fb6b205e118e4395ec0fc2cb5d1d6c1c56dad2
Blanka 3719 <mmohammed>
2026-06-09 14:42:24 +0200

    jndfjn

--- src/database/database.go
+++ src/database/database.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 
 	"forum.com/m/src/config"
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
 func Connect(cfg config.Config) (*sql.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true&charset=utf8mb4&collation=utf8mb4_unicode_ci", cfg.DBUser, cfg.DBPass, cfg.DBHost, cfg.DBName)
-	db, err := sql.Open("mysql", dsn)
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=disable", cfg.DBUser, cfg.DBPass, cfg.DBHost, cfg.DBName)
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
 	}
